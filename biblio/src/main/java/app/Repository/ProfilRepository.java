b8ff2e54409fc50f5f78b531015b9421e6b7be70
RALEVAZAHA Vonjiniaina Emilson <83495723+Ghost135401@users.noreply.github.com>
2025-07-04 08:35:53 +0200

    comit

--- biblio/src/main/java/app/Repository/ProfilRepository.java
+++
@@ -1,7 +0,0 @@
-package app.Repository;
-
-import app.Models.Profil;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfilRepository extends JpaRepository<Profil, Integer> {
-}
--- biblio/src/main/java/app/Repository/StatutRepository.java
+++
@@ -1,7 +0,0 @@
-package app.Repository;
-
-import app.Models.Statut;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatutRepository extends JpaRepository<Statut, Integer> {
-}
