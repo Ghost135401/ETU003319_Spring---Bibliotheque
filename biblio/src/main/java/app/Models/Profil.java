81744735cd6070686f3af9113ccd0afa4fddaffb
RALEVAZAHA Vonjiniaina Emilson <83495723+Ghost135401@users.noreply.github.com>
2025-07-15 12:24:00 +0200

    pom

--- biblio/src/main/java/app/Models/Profil.java
+++ biblio/src/main/java/app/Models/Profil.java
@@ -27,9 +27,33 @@ public class Profil {
         this.nom = nom;
     }
 
-    private Integer quota_emprunt;
-    private Integer duree_emprunt_jours;
+    private int quota_emprunt;
+    public int getQuota_emprunt() {
+        return quota_emprunt;
+    }
+
+    public void setQuota_emprunt(int quota_emprunt) {
+        this.quota_emprunt = quota_emprunt;
+    }
+
+    private int duree_emprunt_jours;
+    public int getDuree_emprunt_jours() {
+        return duree_emprunt_jours;
+    }
+
+    public void setDuree_emprunt_jours(int duree_emprunt_jours) {
+        this.duree_emprunt_jours = duree_emprunt_jours;
+    }
+
     private Integer nb_prolongations_max;
+    public Integer getNb_prolongations_max() {
+        return nb_prolongations_max;
+    }
+
+    public void setNb_prolongations_max(Integer nb_prolongations_max) {
+        this.nb_prolongations_max = nb_prolongations_max;
+    }
+
     private Integer quota_reservations;
 
     @Column(name = "created_at")
@@ -40,5 +64,6 @@ public class Profil {
 
     public Profil() {}
 
- 
+    // Getters and setters
+    // ... (similaires à Adherent)
 }
